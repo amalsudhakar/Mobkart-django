4fae4404997f8783c515134f791561f43f7aa893
AMAL <amalsudhakar52@gmail.com>
2023-09-14 15:00:23 +0530

    Cart with user

--- Accounts/views.py
+++ Accounts/views.py
@@ -5,15 +5,8 @@ from django.contrib import messages, auth
 from functools import wraps
 from django.views.decorators.cache import cache_control
 from django.contrib.auth.decorators import login_required
-
-# verifications
-
-from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import EmailMessage
+from Carts.models import Cart, CartItem
+from Carts.views import _cart_id
 
 
 def redirect_authenticated_user(view_func):
@@ -60,6 +53,44 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None and user.is_admin is False:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+
+                    # getting product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+
+                    # get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:  
+                pass
             auth.login(request, user)
             request.session['email'] = user.email
             request.session['uid'] = user.id
@@ -76,13 +107,13 @@ def login(request):
 
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
-@login_required(login_url = 'login')
+@login_required(login_url='login')
 def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out')
     return redirect('login')
 
 
-@login_required(login_url = 'login')
+@login_required(login_url='login')
 def dashboard(request):
     return render(request, 'Accounts/dashboard.html')
