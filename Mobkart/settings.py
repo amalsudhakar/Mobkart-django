57370a2e8d1ca5c7f70a5ea7d65decda28dc5606
amalsudhakar <amalsudhakar52@gmail.com>
2023-11-02 12:41:50 +0530

    S3 updates

--- Mobkart/settings.py
+++ Mobkart/settings.py
@@ -177,3 +177,15 @@ SESSION_SERIALIZER = "django.contrib.sessions.serializers.JSONSerializer"
 APP_ERROR_VIEW_PERMISSION = 'permission.ErrorViewPermission'
 
 APP_ERROR_USE_DJANGO_ADMIN_SITE = True
+
+# s3 bucket configuration
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_NAME = config('AWS_S3_SIGNATURE_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
